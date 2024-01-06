32ef34e5c936fac145d65e4c242bd39f9f0fc4f5
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2024-01-06 14:17:07 -0600

    Agregar componentes DBZ

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-pages.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
+  //no se exporta list component y add character porque no se va a usar fuera de este modulo
   exports: [MainPageComponent],
   imports: [
     CommonModule
